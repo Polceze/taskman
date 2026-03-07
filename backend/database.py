59b38cdde67c2f6ce940178b8bb81605620dccdc
Polceze <paulo.odera@gmail.com>
2026-03-07 13:48:44 +0300

    chore: updated database.py to detect and allow SSL, for deployment

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config import settings
 
+# Detect if we need SSL (For deployment)
+connect_args = {}
+if "sslmode" in settings.DATABASE_URL:
+    connect_args["sslmode"] = "require"
+
 # Create the SQLAlchemy engine
 engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,   # Verify connections before use
     pool_size=10,
     max_overflow=20,
+    connect_args=connect_args,
 )
 
 # Session factory
